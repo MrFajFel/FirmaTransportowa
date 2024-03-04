4d7bdf65b6702aa7437e77770f3a8361aee6aff6
Student <elkadjk@.pl>
2024-03-04 10:26:28 +0100

    Ale sigma Uuuu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Towar.Kontener;
+import Towar.Magazyn;
+import Towar.Towar;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        //Tworze magazyny
+        Magazyn hamburg = new Magazyn("hamburg");
+        Magazyn tokio = new Magazyn("hamburg");
+        Magazyn wroclaw = new Magazyn("hamburg");
+
+        // 100 kontenerow z konsolami
+        for (int i =0;i<100;i++){
+            tokio.dodajKontener(new Kontener(Towar.KONSOLE));
+        }
+        // 50 kontenerow z bananami
+        for (int i =0;i<100;i++){
+            tokio.dodajKontener(new Kontener(Towar.BANANY));
+        }
+        //40 Kontenerow z meblami
+
+        for (int i =0;i<100;i++){
+            wroclaw.dodajKontener(new Kontener(Towar.MEBLE));
+        }
 
     }
 }
